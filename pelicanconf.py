09a4ec23311180ce6a61637a3cda5dc07e3bf63f
Zac Bir <zbir@zacbir.net>
2022-12-10 19:44:01 -0500

    Favicon support

--- pelicanconf.py
+++ pelicanconf.py
@@ -56,7 +56,7 @@ LIQUID_TAGS = ["img", "include_code"]
 
 DISPLAY_CATEGORIES_ON_MENU = False
 
-STATIC_PATHS = ['code', 'images', '.well-known']
+STATIC_PATHS = ['code', 'images', 'static', '.well-known']
 
 EXTRA_HEADER = '<link rel="me" href="https://dice.camp/@zacbir">'
 
